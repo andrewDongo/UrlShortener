18a630cd5d1234693f5bbcb3d5c93575dd60d2ba
andrewDongo <runyadongo@gmail.com>
2015-11-29 22:23:25 +0800

    Sql Script File

---
+++ urlShortener.sql
@@ -0,0 +1,33 @@
+/*
+SQLyog Community v11.01 (64 bit)
+MySQL - 5.5.25a : Database - url_shortening
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`url_shortening` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `url_shortening`;
+
+/*Table structure for table `urls` */
+
+DROP TABLE IF EXISTS `urls`;
+
+CREATE TABLE `urls` (
+  `id` int(20) unsigned NOT NULL AUTO_INCREMENT,
+  `actualUrl` longtext CHARACTER SET latin1 NOT NULL,
+  `visits` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
